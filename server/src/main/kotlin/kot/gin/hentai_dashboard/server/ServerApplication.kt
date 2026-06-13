e7c2482bc18555981c5efc297c8aec605a45bd2e
AgMonk <31527430+AgMonk@users.noreply.github.com>
2026-06-13 17:55:47 +0800

    init

---
+++ server/src/main/kotlin/kot/gin/hentai_dashboard/server/ServerApplication.kt
@@ -0,0 +1,11 @@
+package kot.gin.hentai_dashboard.server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<ServerApplication>(*args)
+}
---
+++ server/src/test/kotlin/kot/gin/hentai_dashboard/server/ServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package kot.gin.hentai_dashboard.server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
